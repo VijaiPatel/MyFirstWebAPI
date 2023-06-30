0ccfe182df2ef16c42a6cb7d3ccd55b3810e85e0
VijaiPatel <48962282+VijaiPatel@users.noreply.github.com>
2023-06-30 14:02:47 +0100

    Add project files.

---
+++ MyFirstWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyFirstWebAPI.Controllers
+{
+    // ../api/Home
+    public class HomeController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+
+}
